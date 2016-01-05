240c4a9b595133de7472a0086b1a0184e20ebc40
Ed Winn <ed.winn@northstarmutual.com>
2016-01-05 11:02:51 -0600

    Ignore _site directory for docs

--- docs/_site/deploy.bat
+++
@@ -1,3 +0,0 @@
-ssh gitlab.galaxy.local "mkdir -p /var/www/docs/styleguide"
-ssh gitlab.galaxy.local "rm /var/www/docs/styleguide/*"
-scp -r ${PWD}/_site/* gitlab.galaxy.local:/var/www/docs/styleguide
